cb6f8dffcde0edced78fa829b812d162b441de5b
mart8ins <mart8ins@gmail.com>
2023-08-10 17:39:38 +0300

    improved format_sudoku file with rubocop

--- lib/format_sudoku.rb
+++ lib/format_sudoku.rb
@@ -1,26 +1,23 @@
+# frozen_string_literal: true
+
+# Module SudokuFormater
+#
+# This module provides methods to format sudoku game from file input
 module SudokuFormater
+  def clean_sudoku_string_split_lines(sudoku_input)
+    sudoku_input.gsub(/[-+|s]/, '').lines.map(&:chomp).reject(&:empty?)
+  end
 
-    def clean_sudoku_string_split_lines(sudoku_input)
-        return sudoku_input.gsub(/[-+\|s]/, "").lines.map(&:chomp).reject(&:empty?)
+  def create_two_dimensional_array(sudoku_input)
+    cleaned_sudoku = clean_sudoku_string_split_lines(sudoku_input)
+    sudoku_array = []
+    cleaned_sudoku.each_with_index do |row, index|
+      sudoku_array[index] = row.split(' ').map(&:to_i)
     end
+    sudoku_array
+  end
 
-    def create_two_dimensional_array(sudoku_input)
-        cleaned_sudoku = clean_sudoku_string_split_lines(sudoku_input)
-        sudoku_array = []
-        for index in 0..cleaned_sudoku.length - 1
-            sudoku_array[index] = cleaned_sudoku[index].split(" ").map(&:to_i)
-        end
-        return sudoku_array
-    end
-
-    def rotate_array(sudoku_array)
-        rotated_array = Array.new(9) { Array.new }
-        for i in 0..sudoku_array.length - 1
-            for j in 0..sudoku_array[i].length - 1
-                rotated_array[j][i] = sudoku_array[i][j]
-            end
-        end
-        return rotated_array
-    end
-
+  def rotate_array(sudoku_array)
+    sudoku_array.transpose
+  end
 end
