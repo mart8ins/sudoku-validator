b7916f8ba1a5cde6f05c04e4ae8855c975dbb6f0
mart8ins <mart8ins@gmail.com>
2023-08-10 18:19:02 +0300

    improved validator file with rubocop

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,6 +1,10 @@
-require_relative "format_sudoku.rb"
-include SudokuFormater
+# frozen_string_literal: true
 
+require_relative 'format_sudoku'
+include SudokuFormater
+# class Validator
+#
+# Class is used to validate sudoku games
 class Validator
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
@@ -11,69 +15,66 @@ class Validator
   end
 
   def validate
-    sudoku_array =  SudokuFormater.create_two_dimensional_array(@puzzle_string)
+    sudoku_array = SudokuFormater.create_two_dimensional_array(@puzzle_string)
+    not_valid_subgroup, = validate_subgroups(sudoku_array)
     not_valid_row, not_complete_row = validate_rows(sudoku_array)
     not_valid_col, not_complete_col = validate_cols(sudoku_array)
 
-    not_valid_subgroup, not_complete_subgroup = validate_subgroups(sudoku_array)
 
-    if not_valid_subgroup or not_valid_row or not_valid_col
-      return "Sudoku is invalid."
-    elsif !not_valid_row and not_complete_row or !not_valid_col and not_complete_col
-      return "Sudoku is valid but incomplete."
+
+    if not_valid_subgroup || not_valid_row || not_valid_col
+      'Sudoku is invalid.'
+    elsif !not_valid_row && not_complete_row || !not_valid_col && not_complete_col
+      'Sudoku is valid but incomplete.'
     else
-      return "Sudoku is valid."
+      'Sudoku is valid.'
     end
   end
 
   def validate_cols(sudoku_array)
-      rotated_array = SudokuFormater.rotate_array(sudoku_array)
-      return validate_rows(rotated_array)
+    rotated_array = SudokuFormater.rotate_array(sudoku_array)
+    validate_rows(rotated_array)
   end
-  
+
   def validate_rows(sudoku_array)
     not_valid = false
     not_complete = false
     numbers = []
 
-    for sudoku_line in sudoku_array
-        if not_valid
+    sudoku_array.each do |sudoku_line|
+      break if not_valid
+
+
+      sudoku_line.each do |number_in_line|
+        next unless number_in_line != 0
+
+        if (numbers.include? number_in_line) || number_in_line.negative? || (number_in_line > 9)
+          not_valid = true
           break
-        else
-          for number_in_line in sudoku_line
-            if number_in_line != 0
-              if numbers.include? number_in_line or number_in_line < 0 or number_in_line > 9
-                not_valid = true
-                break
-              end
-              numbers.push(number_in_line)
-            end
         end
-          if sudoku_line.include? 0
-            not_complete = true
-          end
-        numbers = []
-        end
+        numbers.push(number_in_line)
+      end
+      not_complete = true if sudoku_line.include? 0
+      numbers = []
     end
-    return not_valid, not_complete
+    [not_valid, not_complete]
   end
 
   def validate_subgroups(sudoku_array)
     subgroups = []
 
-    subgroups.push(get_sub_group(0,2,sudoku_array))
-    subgroups.push(get_sub_group(3,5,sudoku_array))
-    subgroups.push(get_sub_group(6,8,sudoku_array))
+    subgroups.push(get_sub_group(0, 2, sudoku_array))
+    subgroups.push(get_sub_group(3, 5, sudoku_array))
+    subgroups.push(get_sub_group(6, 8, sudoku_array))
 
-    return validate_rows(subgroups)
+    validate_rows(subgroups)
   end
 
   def get_sub_group(start_index, end_index, array)
     group = []
-    for i in start_index..end_index
-      group.push(*array[i][start_index,3])
+    (start_index..end_index).each do |i|
+      group.push(*array[i][start_index, 3])
     end
-    return group
+    group
   end
-
 end
